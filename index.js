45b81e1e93d453327daba0d39e74aa56f62ab80b
Aloush-i <ali.mon.a.1250@gmail.com>
2024-11-15 23:42:16 +0200

    add a total sum and some styles

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
-let count = 0
+let sumEl = document.getElementById("sum-el")
 
+let count = 0
+let totalSum = 0 // intialization total sum
 function increment() {
     count += 1
     countEl.textContent = count
+    
 }
 
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
+    totalSum += count // update total sum with current count
+    sumEl.textContent = "Total: " + totalSum; //Display the tptal sum
     countEl.textContent = 0
     count = 0
+    
 }
+
